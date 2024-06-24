6fcdcf31b6c36e40b26c4960a56b0535935ba029
TimSimon66 <huynhvantin125ttt@gmail.com>
2024-06-24 19:22:03 +0700

    video 31, su dung bien so voi JSX, dung JSON.stringify(bien so) de convert bien soarray, object sang dang string va display ra screen

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,20 @@
 // component = html + css + js
 import './style.css'
 const MyComponent = () => {
+    //const hoidanit = "eric 1" //string
+    //const hoidanit = 25 //number
+    //const hoidanit = true //boolean
+    //const hoidanit = undefined //
+    //const hoidanit = null //boolean
+    const hoidanit = [1, 2, 3] //array
+    // const hoidanit = {
+    //     name: 'hoidanit',
+    //     age: 25
+    // }
     return (
         <>
-            <div> eric & hoidanit update </div>
+            <div> {JSON.stringify(hoidanit)} & hoidanit update </div>
+            <div>{console.log("ERIC")}</div>
             <div className="child"
                 style={
                     { borderRadius: "10px" }
