e27e5a42a21640cdd838bc610dd7eb71a7588000
TimSimon66 <huynhvantin125ttt@gmail.com>
2024-06-28 20:10:50 +0700

    video 34, props, ta su dung props de truyen du lieu tu cha sang componet con

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,22 @@ import reactLogo from './assets/react.svg'
 
 const App = () => {
 
+  const hoidanit = "Eric Arsenan";
+  const age = 25;
+  const data = {
+    address: "ha noi",
+    country: "vietnam",
+  }
+
   return (
     <div className="todo-container">
       <div className="todo-title">Todo list</div>
       <TodoNew />
-      <TodoData />
+      <TodoData
+        name={hoidanit}
+        age={age}
+        data={data}
+      />
       <div className="todo-image">
         <img src={reactLogo} className="logo" />
       </div>
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,7 +1,11 @@
 
-const TodoData = () => {
+const TodoData = (props) => {
+    //props la bien object
+    console.log(">> check props: ", props)
+    const { name, age, data } = props;
     return (
         <div className="todo-data">
+            <div>My name is {name}</div>
             <div>Learning React</div>
             <div>Watching Youtube</div>
         </div>
