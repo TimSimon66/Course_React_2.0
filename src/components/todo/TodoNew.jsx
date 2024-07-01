ea59774a2e438b5fe46a3a4442e7ae1b75a5dce0
TimSimon66 <huynhvantin125ttt@gmail.com>
2024-07-01 18:29:54 +0700

    video36, su dung su kien onChange, su dung arrow function cho su kien onChange, de tranh viec goi ham

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -3,11 +3,24 @@ const TodoNew = (props) => {
     console.log(">>> check point: ", props);
     const { addNewTodo } = props;
 
+    const handleClick = () => {
+        alert("Click me");
+    }
+
+    const handleOnChange = (name) => {
+        console.log(">>> handleOnChange", name);
+    }
+
     addNewTodo("eric");
     return (
         <div className="todo-new">
-            <input type="text" />
-            <button>Add</button>
+            <input type="text"
+                onChange={(event) => handleOnChange(event.target.value)}
+            />
+            <button
+                style={{ cursor: "pointer" }}
+                onClick={handleClick}
+            >Add</button>
         </div>
     )
 }
