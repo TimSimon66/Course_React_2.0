44af49aa18c89968485f06656231a2002b91aa8d
TimSimon66 <huynhvantin125ttt@gmail.com>
2024-07-04 10:28:33 +0700

    video 39, render du lieu voi vong lap map

--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -2,13 +2,21 @@
 const TodoData = (props) => {
     //props la bien object
     console.log(">> check props: ", props)
-    const { name, age, data } = props;
+    const { todoList } = props;
     console.log(">>check data", props)
     return (
         <div className="todo-data">
-            <div>My name is {name}</div>
-            <div>Learning React</div>
-            <div>Watching Youtube</div>
+            {todoList.map((item, index) => {
+                console.log(">> check ", item, index)
+                return (
+                    <div>
+                        <div className="todo-itme">
+                            {item.name}
+                            <button>Delete</button>
+                        </div>
+                    </div>
+                )
+            })}
             <div>
                 {JSON.stringify(props.todoList)}
             </div>
