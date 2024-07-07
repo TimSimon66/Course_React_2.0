55e87b1e1c6c401a889e73d5dbdefc3f654e39f5
TimSimon66 <huynhvantin125ttt@gmail.com>
2024-07-07 20:47:02 +0700

    video 40, them key cho cac data, de hoi co the xoa data

--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,25 +1,20 @@
 
 const TodoData = (props) => {
     //props la bien object
-    console.log(">> check props: ", props)
     const { todoList } = props;
-    console.log(">>check data", props)
     return (
         <div className="todo-data">
             {todoList.map((item, index) => {
                 console.log(">> check ", item, index)
                 return (
                     <div>
-                        <div className="todo-itme">
-                            {item.name}
+                        <div className={`todo-item ${index}`} key={index.id}>
+                            <div> {item.name} </div>
                             <button>Delete</button>
                         </div>
                     </div>
                 )
             })}
-            <div>
-                {JSON.stringify(props.todoList)}
-            </div>
         </div>
     )
 }
