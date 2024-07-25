e4ddfabae25060dfe36942947c537f6008905db1
TimSimon66 <huynhvantin125ttt@gmail.com>
2024-07-25 10:12:38 +0700

    video 67, sử dụng React useState để quản lí dữ liệu từ các form input như fullname, password, email, phonNumber. Sử dụng thuộc tính value để nhận giá trị ban đầu, và sử dụng hàm onChange truyền vào hàm set để set lại các giá trị ban đầu

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,29 +1,50 @@
 import { Input } from "antd";
 import { Button, Flex } from 'antd';
+import { useState } from "react";
 const UserForm = () => {
-
+    const [fullName, setFullName] = useState("")
+    const [email, setFullEmail] = useState("")
+    const [password, setFullPassword] = useState("")
+    const [phoneNumber, setFullPhoneNumber] = useState("")
+    const handleClickBtn = () => {
+        console.log(">>>> check ", { fullName, email, password, phoneNumber })
+    }
     return (
         <div className="user-form" style={{ margin: "20px 0" }}>
             <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
                 <div>
                     <span>FullName</span>
-                    <Input />
+                    <Input
+                        value={fullName}
+                        onChange={(event) => setFullName(event.target.value)}
+                    />
                 </div>
                 <div>
                     <span>Email</span>
-                    <Input />
+                    <Input
+                        value={email}
+                        onChange={(event) => setFullEmail(event.target.value)}
+
+                    />
                 </div>
                 <div>
                     <span>Password</span>
-                    <Input.Password />
+                    <Input.Password
+                        value={password}
+                        onChange={(event) => setFullPassword(event.target.value)}
+
+                    />
                 </div>
                 <div>
                     <span>Phone number</span>
-                    <Input />
+                    <Input
+                        value={phoneNumber}
+                        onChange={(event) => setFullPhoneNumber(event.target.value)}
+                    />
                 </div>
             </div>
             <div>
-                <Button type="primary"> Create User </Button>
+                <Button type="primary" onClick={handleClickBtn}> Create User </Button>
             </div>
         </div>
     )
